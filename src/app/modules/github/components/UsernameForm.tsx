6b00800adf034d1049cb5db965e105bf852d8742
Shrey Koradia <shrey.koradia@yahoo.com>
2024-06-12 09:22:32 +0530

    addition of top github repositories

--- src/app/modules/github/components/UsernameForm.tsx
+++ src/app/modules/github/components/UsernameForm.tsx
@@ -2,50 +2,61 @@
 
 import Loader from "@/app/shared/components/Loader";
 import axios from "axios";
-import { useState } from "react";
+import { Dispatch, SetStateAction, useState } from "react";
+import { GithubRepoResponseProp } from "../types";
 
 interface UsernameFormProps {
   onRoastAction?: (roast: string) => void;
+  isLoading: boolean;
+  username: string;
+  setUsername: Dispatch<SetStateAction<string>>;
+  handleLoadingAction: (isLoading: boolean) => void;
+  handleGithubData: (githubData: GithubRepoResponseProp[]) => void;
 }
-const UsernameForm: React.FC<UsernameFormProps> = ({ onRoastAction }) => {
-  const [username, setUsername] = useState<string>("");
-  const [isLoading, setIsLoading] = useState<boolean>(false);
+const UsernameForm: React.FC<UsernameFormProps> = ({
+  onRoastAction,
+  isLoading,
+  handleLoadingAction,
+  handleGithubData,
+  username,
+  setUsername,
+}) => {
   const handleRoastBtn = async () => {
     try {
-      setIsLoading(true);
+      handleLoadingAction;
       const res = await axios.post("/api/github", { username });
       if (res?.data) {
-        // onRoastAction(res?.data?.roast);
-        setIsLoading(false);
+        handleGithubData(res?.data?.github_repo_data);
+        handleLoadingAction(!isLoading);
       }
       setUsername("");
     } catch (error) {
+      handleLoadingAction(!isLoading);
       console.log(error);
     }
   };
-  if (isLoading) {
-    return <Loader />;
-  }
   return (
-    <div className="flex justify-center items-center w-full gap-4">
-      <input
-        type="text"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-        placeholder="Enter GitHub username"
-        className="p-2 border bg-transparent rounded"
-      />
-      <button
-        type="button"
-        className="text-divamecha hover:text-crimsonred hover:border-b hover:border-b-crimsonred font-bold"
-        onClick={(e) => {
-          e.preventDefault();
-          handleRoastBtn();
-        }}
-        disabled={isLoading}
-      >
-        Roast Me!
-      </button>
+    <div className="flex justify-center items-center">
+      <div className="flex justify-center items-center gap-2">
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          placeholder="Enter GitHub username"
+          className="p-2 border-b focus:outline-none bg-transparent"
+        />
+        <button
+          type="button"
+          className="text-divamecha hover:text-crimsonred hover:border-b hover:border-b-crimsonred font-bold"
+          onClick={(e) => {
+            e.preventDefault();
+            handleRoastBtn();
+          }}
+          disabled={isLoading}
+        >
+          Roast Me!
+        </button>
+      </div>
     </div>
   );
 };
--- src/app/modules/github/index.tsx
+++ src/app/modules/github/index.tsx
@@ -3,11 +3,30 @@
 import React, { useState } from "react";
 import axios from "axios";
 import UsernameForm from "./components/UsernameForm";
+import Typewriter from "typewriter-effect";
 import ChatWindow from "@/app/shared/components/ChatWindow";
 import InputForm from "@/app/shared/components/InputForm";
+import Loader from "@/app/shared/components/Loader";
+import { GithubRepoResponseProp } from "./types";
 
 const GithubModule: React.FC = () => {
   //   const [conversation, setConversation] = useState<string[]>([]);
+
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [username, setUsername] = useState<string>("");
+
+  const [githubRepoData, setGithubRepoData] = useState<
+    GithubRepoResponseProp[]
+  >([]);
+
+  const topGithubRepo = () => {
+    let repoName = [];
+    for (let i = 0; i < 10; i++) {
+      if (githubRepoData[i]?.name === null || undefined) return;
+      repoName.push(githubRepoData[i]?.name);
+    }
+    return repoName;
+  };
 
   //   const handleRoastReceived = (roast: string) => {
   //     setConversation((prev) => [...prev, `Bot: ${roast}`]);
@@ -19,16 +38,53 @@ const GithubModule: React.FC = () => {
   //     setConversation((prev) => [...prev, `Bot: ${res.data.response}`]);
   //   };
 
+  if (isLoading) {
+    return <Loader />;
+  }
+
   return (
     <main>
       <div className="grid place-items-center place-content-center h-[100vh]">
-        <h3 className="text-6xl font-mono font-bold mb-6">Roast My Github!</h3>
-        <UsernameForm />
-        {/* <ChatWindow conversation={conversation} />
-        <InputForm onMessageSubmit={handleMessageSubmit} /> */}
+        {githubRepoData.length === 0 ? (
+          <div>
+            <h3 className="text-6xl font-mono font-bold mb-6">
+              Roast My Github!
+            </h3>
+            <UsernameForm
+              isLoading={isLoading}
+              handleLoadingAction={(isLoading: boolean) =>
+                setIsLoading(!isLoading)
+              }
+              handleGithubData={(githubData: GithubRepoResponseProp[]) =>
+                setGithubRepoData(githubData)
+              }
+              username={username}
+              setUsername={setUsername}
+            />
+          </div>
+        ) : (
+          <Typewriter
+            onInit={(typewriter) => {
+              typewriter
+                .typeString(
+                  `Hi, Your Top Github Repositories are ${topGithubRepo()?.map(
+                    (item: string) => item
+                  )}
+                `
+                )
+                .pauseFor(1000)
+                .start();
+            }}
+          />
+        )}
       </div>
     </main>
   );
+
+  {
+    /* <ChatWindow conversation={conversation} />
+        <InputForm onMessageSubmit={handleMessageSubmit} /> */
+  }
 };
 
 export default GithubModule;
